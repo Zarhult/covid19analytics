d347405d3d56fb1f74e3109cc22245233a34a200
Calvin Glisson <cglis001@ucr.edu>
2020-04-17 11:09:53 -0700

    Parser complete

--- src/Server/Server/Parser.cs
+++ src/Server/Server/Parser.cs
@@ -3,19 +3,16 @@ using System.IO;
 using System.Text;
 using System.Collections.Generic;
 using System.Globalization;
-//For debugging
-using System.Diagnostics;
 
-// Ignoring inconsistent "last updated" data to keep things simpler
+// Ignoring observation date and serial number
 
 namespace Server
 {
     public class COVIDDataPoint
     {
-        public int num = 0;
-        public String obsDate = "";
-        public String city = "";
+        public String city = ""; // For some data points this is left blank and will remain an empty string
         public String country = "";
+        public String lastUpdate = "";
         public double confirmed = 0;
         public double dead = 0;
         public double recovered = 0;
@@ -25,11 +22,11 @@ namespace Server
     {
         public static List<COVIDDataPoint> ParseCSV(String fileName)
         {
+            List<COVIDDataPoint> data = new List<COVIDDataPoint>();
+
             using (StreamReader sr = new StreamReader(fileName))
             {
-                List<COVIDDataPoint> data = new List<COVIDDataPoint>();
-
-                // Skip to the data
+                // Skip first line to get to the data
                 sr.ReadLine();
 
                 while (!sr.EndOfStream)
@@ -37,33 +34,31 @@ namespace Server
                     String line = sr.ReadLine();
                     String[] values;
 
-                    if (!line.Contains("\""))
-                    {
-                        values = line.Split(',');
-                    }
-                    else
-                    {
-                        // Handle quoted data here
-                    }
+                    values = line.Split(',');
 
                     COVIDDataPoint point = new COVIDDataPoint();
 
-                    // Skipping index 4 since ignoring "last updated" for now
-                    point.num = Int32.Parse(values[0]);
-                    point.obsDate = values[1];
-                    point.city = values[2];
-                    point.country = values[3];
-                    //point.confirmed = double.Parse(values[5], CultureInfo.InvariantCulture);
-                    //point.dead = double.Parse(values[6]);
-                    //point.recovered = double.Parse(values[7]);
-
-                    //Debug.WriteLine(values[5]);
+                    int quoteOffset = 0; // Adjust indices accordingly for city data like "Los Angeles, CA"
+                    if (line.Contains("\""))
+                    {
+                        quoteOffset = 1;
+                        point.city = values[2] + ", " + values[3];
+                    }
+                    else
+                    {
+                        point.city = values[2];
+                    }
+                    point.country = values[3 + quoteOffset];
+                    point.lastUpdate = values[4 + quoteOffset];
+                    point.confirmed = double.Parse(values[5 + quoteOffset], CultureInfo.InvariantCulture);
+                    point.dead = double.Parse(values[6 + quoteOffset], CultureInfo.InvariantCulture);
+                    point.recovered = double.Parse(values[7] + quoteOffset, CultureInfo.InvariantCulture);
 
                     data.Add(point);
                 }
-
-                return data;
             }
+
+            return data;
         }
     }
 }
--- src/Server/Server/Program.cs
+++ src/Server/Server/Program.cs
@@ -14,10 +14,23 @@ namespace Server
         [STAThread]
         static void Main()
         {
-            List<COVIDDataPoint> data = Parser.ParseCSV("D:\\data\\covid_19_data.csv");
+            List<COVIDDataPoint> data = Parser.ParseCSV("<FILENAME>");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            // For testing the parser
+            /*
+            foreach(COVIDDataPoint point in data)
+            {
+                System.Diagnostics.Debug.WriteLine(point.city);
+                System.Diagnostics.Debug.WriteLine(point.country);
+                System.Diagnostics.Debug.WriteLine(point.lastUpdate);
+                System.Diagnostics.Debug.WriteLine(point.confirmed);
+                System.Diagnostics.Debug.WriteLine(point.dead);
+                System.Diagnostics.Debug.WriteLine(point.recovered);
+            }
+            */
         }
     }
 }
