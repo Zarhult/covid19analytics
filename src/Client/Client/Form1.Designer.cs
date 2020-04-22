1356a2dc52e5ff2be7390fb145a57d4efbe3ef85
ehalf001 <38255772+ehalf001@users.noreply.github.com>
2020-04-22 03:13:04 -0700

    Client Files

--- src/Client/Client/Form1.Designer.cs
+++ src/Client/Client/Form1.Designer.cs
@@ -33,15 +33,28 @@
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
             this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.linkLabel2 = new System.Windows.Forms.LinkLabel();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(306, 95);
+            this.button1.Location = new System.Drawing.Point(12, 31);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(102, 36);
             this.button1.TabIndex = 0;
@@ -51,7 +64,7 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(491, 104);
+            this.textBox1.Location = new System.Drawing.Point(222, 40);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 1;
@@ -59,7 +72,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(440, 107);
+            this.label1.Location = new System.Drawing.Point(160, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(26, 13);
             this.label1.TabIndex = 2;
@@ -67,36 +80,28 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(106, 189);
+            this.textBox2.Location = new System.Drawing.Point(12, 105);
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(624, 167);
+            this.textBox2.Size = new System.Drawing.Size(341, 217);
             this.textBox2.TabIndex = 3;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(103, 160);
+            this.label2.Location = new System.Drawing.Point(151, 77);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.Size = new System.Drawing.Size(25, 13);
             this.label2.TabIndex = 4;
-            this.label2.Text = "Messages";
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(106, 377);
-            this.textBox3.Multiline = true;
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(507, 56);
-            this.textBox3.TabIndex = 5;
+            this.label2.Text = "Log";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(630, 377);
+            this.button2.Location = new System.Drawing.Point(618, 370);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(100, 54);
             this.button2.TabIndex = 6;
-            this.button2.Text = "Send";
+            this.button2.Text = "Search";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
@@ -108,20 +113,210 @@
             // 
             this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(464, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Date";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(500, 74);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(641, 74);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(464, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Age";
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(500, 128);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 20);
+            this.textBox5.TabIndex = 12;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(641, 128);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 20);
+            this.textBox6.TabIndex = 13;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(464, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Gender";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(512, 175);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(49, 17);
+            this.checkBox1.TabIndex = 15;
+            this.checkBox1.Text = "Male";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(587, 176);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(60, 17);
+            this.checkBox2.TabIndex = 16;
+            this.checkBox2.Text = "Female";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(464, 221);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(51, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Countries";
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.AllowDrop = true;
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Items.AddRange(new object[] {
+            "Afghanistan",
+            "Algeria",
+            "Austrailia",
+            "Austria",
+            "Bahrain",
+            "Belgium",
+            "Brazil",
+            "Cambodia",
+            "Canada",
+            "China",
+            "Croatia",
+            "Equador",
+            "Egypt",
+            "Estonia",
+            "Finland",
+            "France",
+            "Georgia",
+            "Germany",
+            "Greece",
+            "India",
+            "Iran",
+            "Iraq",
+            "Israel",
+            "Italy",
+            "Japan",
+            "Kuwait",
+            "Lebanon",
+            "Lithuania",
+            "Malaysia",
+            "Nepal",
+            "Netherlands",
+            "Nigeria",
+            "North Macedonia",
+            "Norway",
+            "Oman",
+            "Pakistan",
+            "Philippines",
+            "Romania",
+            "Russia",
+            "San Marino",
+            "Singapore",
+            "South Korea",
+            "Spain",
+            "Sri Lanka",
+            "Sweden",
+            "Switzerland",
+            "Thailand",
+            "United Arab Emirates",
+            "United Kingdom",
+            "United States",
+            "Vietnam"});
+            this.checkedListBox1.Location = new System.Drawing.Point(461, 237);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(120, 199);
+            this.checkedListBox1.TabIndex = 18;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(521, 221);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(18, 13);
+            this.linkLabel1.TabIndex = 19;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "All";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // linkLabel2
+            // 
+            this.linkLabel2.AutoSize = true;
+            this.linkLabel2.Location = new System.Drawing.Point(548, 221);
+            this.linkLabel2.Name = "linkLabel2";
+            this.linkLabel2.Size = new System.Drawing.Size(33, 13);
+            this.linkLabel2.TabIndex = 20;
+            this.linkLabel2.TabStop = true;
+            this.linkLabel2.Text = "None";
+            this.linkLabel2.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel2_LinkClicked);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 379);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(102, 36);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Import Data";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.linkLabel2);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "X";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -134,10 +329,23 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Button button2;
         private System.ComponentModel.BackgroundWorker backgroundWorker1;
         private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.LinkLabel linkLabel2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
--- src/Client/Client/Form1.cs
+++ src/Client/Client/Form1.cs
@@ -22,12 +22,72 @@ namespace Client
         public StreamWriter STW;
         public string recieve;
         public String text_to_send;
+        public String date;
+        public String age;
+        public String gender;
+        public String countries;
+        public String SendMessage;
         public Form1()
         {
             InitializeComponent();
         }
 
+        public bool dateCheck(String date)
+        {
+            if (date == "") return true;
+            if (date.Length != 8) return false; //Make sure it is correct format
+            if (date[0] > '1' || date[0] < '0') return false; //Tens place check
+            if (date[0] == '0' && (date[1] > '9' || date[1] < '0')) return false; //Single digit dates
+            if (date[0] == '1' && (date[1] > '2' || date[1] < '0')) return false; //Double digit dates
+            if (date[2] != '/') return false; //Format
+            if (date[3] > '3' || date[3] < '0') return false; //Tens place
+            if (date[3] == '0' && (date[4] > '9' || date[4] < '0')) return false;
+            if (date[3] == '1' && (date[4] > '9' || date[4] < '0')) return false;
+            if (date[3] == '3' && (date[4] > '1' || date[4] < '0')) return false;
+            if (date[5] != '/') return false;
+            if (date[6] > '2' || date[6] < '0') return false;
+            if (date[6] == '0' && (date[7] > '9' || date[7] < '0')) return false;
+            if (date[6] == '1' && (date[7] > '9' || date[7] < '0')) return false;
+            if (date[6] == '2' && date[7] != '0') return false;
+            return true;
+        }
 
+        public bool dateOrient(String date1, String date2) //mm/dd/yy
+        {
+            if (date1 == "") return true;
+            if (date2 == "") return true;
+            if (date1[6] > date2[6]) return false;
+            if (date1[6] == date2[6] && date1[7] > date2[7]) return false;
+            if (date1[0] > date2[0]) return false;
+            if (date1[0] == date2[0] && date1[1] > date2[1]) return false;
+            if (date1[3] > date2[3]) return false;
+            if (date1[3] == date2[3] && date1[4] > date2[4]) return false;
+            return true;
+        }
+
+        public bool ageCheck(String age)
+        {
+            if (age == "") return true;
+            if (age.Length > 3) return false;
+            if (age.Length == 3 && (age[0] != '1' || (age[1] > '2' || age[1] < '0') || (age[2] > '9' || age[2] < '0'))) return false;
+            if (age.Length == 2 && ((age[0] > '9' || age[0] < '1') || (age[1] > '9' || age[1] < '0'))) return false;
+            if (age.Length == 1 && (age[0] > '9' || age[0] < '1')) return false;
+            return true;
+        }
+
+        public bool ageOrient(String age1, String age2)
+        {
+            if (age1 == "") return true;
+            if (age2 == "") return true;
+            if (age2.Length < age1.Length) return false;
+            if (age2.Length > age1.Length) return true;
+            if (age2.Length == 3 && age2[1] < age1[1]) return false;
+            if (age2.Length == 3 && age2[2] < age1[2]) return false;
+            if (age2.Length == 2 && age2[1] < age1[1]) return false;
+            if (age2.Length == 2 && age2[2] < age1[2]) return false;
+            if (age2[0] < age1[0]) return false;
+            return true;
+        }
         private void button1_Click(object sender, EventArgs e) //Connection
         {
             string IP = "";
@@ -85,7 +145,7 @@ namespace Client
             if (client.Connected)
             {
                 STW.WriteLine(text_to_send);
-                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("Client Requests: " + text_to_send + "\n"); }));
+                this.textBox2.Invoke(new MethodInvoker(delegate () { textBox2.AppendText("Client Requests: " + SendMessage + "\n"); }));
 
             }
             else
@@ -97,12 +157,91 @@ namespace Client
 
         private void button2_Click(object sender, EventArgs e) //Send Information LOOK HERE
         {
-            if (textBox3.Text != "")
+            text_to_send = "";
+            //Dates
+            if (dateCheck(textBox3.Text) && dateCheck(textBox4.Text) && dateOrient(textBox3.Text, textBox4.Text))
             {
-                text_to_send = textBox3.Text; //--> INFORMATION SENT <--
-                backgroundWorker2.RunWorkerAsync();
+                if ((textBox3.Text == "") && (textBox4.Text == ""))
+                {
+                    date = "All Dates";
+                }
+                else if ((textBox3.Text == ""))
+                {
+                    date = textBox4.Text + " from"; //13
+                }
+                else if ((textBox4.Text == ""))
+                {
+                    date = textBox3.Text + " to"; //11
+                }
+                else
+                {
+                    date = textBox3.Text + " - " + textBox4.Text;
+                }
+
+                //Age
+                if (ageCheck(textBox5.Text) && ageCheck(textBox6.Text) && ageOrient(textBox5.Text, textBox6.Text))
+                {
+                    if ((textBox5.Text == "") && (textBox6.Text == ""))
+                    {
+                        age = "All Ages";
+                    }
+                    else if ((textBox5.Text == ""))
+                    {
+                        age = textBox6.Text + " from"; 
+                    }
+                    else if ((textBox6.Text == ""))
+                    {
+                        age = textBox5.Text +  " to";
+                    }
+                    else
+                    {
+                        age = textBox5.Text + " - " + textBox6.Text; 
+                    }
+                    textBox3.Text = "";
+                    textBox4.Text = "";
+                    textBox5.Text = "";
+                    textBox6.Text = "";
+
+
+                    //Gender
+                    if (checkBox1.Checked == false && checkBox2.Checked == false) gender = "All Genders";
+                    if (checkBox1.Checked == true && checkBox2.Checked == false) gender = "Male Only";
+                    if (checkBox1.Checked == false && checkBox2.Checked == true) gender = "Female Only";
+                    if (checkBox1.Checked == true && checkBox2.Checked == true) gender = "All Genders";
+                    checkBox1.Checked = false;
+                    checkBox2.Checked = false;
+
+                    //Countries
+                    countries = "";
+                    for (int i = 0; i < checkedListBox1.CheckedItems.Count; i++)
+                        countries += checkedListBox1.CheckedItems[i].ToString() + ' ';
+                    if (countries == "") countries = "All Countries";
+                    backgroundWorker2.RunWorkerAsync();
+                    text_to_send = date + "," + age + "," + gender + "," + countries;
+                    SendMessage = date + ", " + age + ", " + gender + ", " + countries;
+                }
+                else
+                {
+                    MessageBox.Show("Invalid Age");
+                }
             }
-            textBox3.Text = "";
+            else
+            {
+                MessageBox.Show("Invalid Date");
+            }
+
+
         }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++) checkedListBox1.SetItemChecked(i, true);
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            for (int i = 0; i < checkedListBox1.Items.Count; i++) checkedListBox1.SetItemChecked(i, false);
+        }
+
     }
 }
